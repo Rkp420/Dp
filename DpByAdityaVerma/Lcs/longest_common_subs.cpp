9b27153fb2fc3975a18b40353af6249ff5ef7bec
Rkp420 <rkp20442@gmail.com>
2025-04-07 00:39:29 +0530

    Started Lcs Using Dp by Solving Lcs

---
+++ DpByAdityaVerma/Lcs/longest_common_subs.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+// Top - Down 
+class Solution
+{
+public:
+    vector<vector<int>> dp;
+    int solve(string s1, string s2, int n, int m)
+    {
+        if (n == 0 || m == 0)
+            return 0;
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        if (s1[n - 1] == s2[m - 1])
+            return dp[n][m] = 1 + solve(s1, s2, n - 1, m - 1);
+
+        else
+        {
+            return dp[n][m] = max(solve(s1, s2, n, m - 1), solve(s1, s2, n - 1, m));
+        }
+    }
+
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n = text1.size();
+        int m = text2.size();
+
+        dp.clear();
+        dp.resize(n + 1, vector<int>(m + 1, -1));
+        return solve(text1, text2, n, m);
+    }
+};
+
+// Bottom - Up Approach
+
+class Solution
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(text1[i - 1] == text2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
+
+
+int main()
+{
+    return 0;
+}
